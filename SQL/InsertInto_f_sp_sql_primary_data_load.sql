a779795464febd87825941dccfe4cfc013c26295
Врубель Ксения Сергеевна <vrubel@proitr.ru>
2016-07-04 17:55:20 +0500

    таск 1038, поправила T_lot_member

--- SQL/InsertInto_f_sp_sql_primary_data_load.sql
+++ SQL/InsertInto_f_sp_sql_primary_data_load.sql
@@ -5008,7 +5008,7 @@ END;#';
     rec_array(idx).is_actual := 1;
     rec_array(idx).sql_text := start_str || q'#
 
-        INSERT INTO REPORTS.T_LOT_MEMBER (ID,
+INSERT INTO REPORTS.T_LOT_MEMBER (ID,
                                            ID_LOT,
                                            ID_SUPPLIER,
                                            REQUEST_NUMBER,
@@ -5021,7 +5021,7 @@ END;#';
                                            GRBS)
 
 
-          select distinct
+select distinct
             --d.*,
             win.ID||4||(CASE WHEN l.joint_auction=1 or l.included_joint_auction=1 then l.id end), 
             l.id lot_id, 
@@ -5033,22 +5033,23 @@ END;#';
                  when d.admitted=1 and d.admitted_second is null then 1
                  else 2 end) over (partition by win.id) as STATE,
             4 METHOD_OF_SUPPLIER_ID,
-            V_id_Data_source, 
+            V_ID_DATA_SOURCE, 
             V_VERSION_DATE, 
             grbs_code
-          from D_EA_APPLICATION@eaist_mos_shard d   
-          join(select distinct win.*, rank() over (partition by journal_number, oos_reg_number, oos_ftp_protocol_id order by price, added_at desc) rnk
+from
+  (select distinct win.*, t.id tender_id, rank() over (partition by journal_number, oos_reg_number, oos_ftp_protocol_id order by price, added_at desc) rnk
                 from d_ea_winner@eaist_mos_shard win 
-                join d_procedure_entity@eaist_mos_shard pe on win.procedure_id=pe.id and pe.deleted_date is null and pe.published_id is not null) win
-          on d.journal_number=win.journal_number and d.oos_reg_number=win.oos_reg_number and d.oos_ftp_protocol_id=win.oos_ftp_protocol_id and win.rnk=1
-          join (select id, registry_number from t_tender where version_date=V_VERSION_DATE and id_data_source=V_ID_DATA_SOURCE) t on d.oos_reg_number=t.registry_number
-          join (select id, tender_Id, version_date, customer_id, joint_auction, included_joint_auction from t_lot where version_date=V_VERSION_DATE and id_data_source=V_ID_DATA_SOURCE) l on l.tender_id=t.id
-          left join (select lcag.id lnk_id, cust.grbs_code, lcag.version_date
-                     from LNK_CUSTOMERS_ALL_LEVEL lcag
-                    join sp_customer cust ON lcag.id_parent=cust.id||'_'||cust.id_DATA_source and lcag.version_date=cust.version_date and cust.connect_level=3
-                    union
-                    select id||'_2', grbs_code, version_date from sp_customer where connect_level=3 and id_Data_source=V_ID_DATA_SOURCE) lcag  
-                    ON l.CUSTOMER_ID||'_2'=lcag.lnk_id and l.version_date=lcag.version_date ;
+                join t_tender t on win.procedure_id=t.entity_id and t.id_data_source=V_ID_DATA_SOURCE and t.version_date=V_VERSION_DATE) win 
+  join (select id, tender_Id, version_date, customer_id, joint_auction, included_joint_auction from t_lot where version_date=V_VERSION_DATE and id_data_source=V_ID_DATA_SOURCE) l on l.tender_id=win.tender_id
+  and win.rnk=1
+  left join D_EA_APPLICATION@eaist_mos_shard d  
+          on d.journal_number=win.journal_number and d.oos_reg_number=win.oos_reg_number and d.oos_ftp_protocol_id=win.oos_ftp_protocol_id          
+  left join (select lcag.id lnk_id, cust.grbs_code, lcag.version_date
+             from LNK_CUSTOMERS_ALL_LEVEL lcag
+             join sp_customer cust ON lcag.id_parent=cust.id||'_'||cust.id_DATA_source and lcag.version_date=cust.version_date and cust.connect_level=3
+               union
+              select id||'_2', grbs_code, version_date from sp_customer where connect_level=3 and id_Data_source=2) lcag  
+   ON l.CUSTOMER_ID||'_2'=lcag.lnk_id and l.version_date=lcag.version_date;
 
     -- Привязка кол-ва обработанных строк
     :V_ROWCOUNT := SQL%ROWCOUNT;
@@ -5070,14 +5071,21 @@ END;#';
                                            PRICE,
                                            ID_DATA_SOURCE,
                                            VERSION_DATE)
-          select distinct win.ID||4||(CASE WHEN l.joint_auction=1 or l.included_joint_auction=1 then l.id end) winid, win.ID||4, d.APP_DATE, win.PRICE, V_ID_DATA_SOURCE id_Data_source, V_VERSION_DATE version_date 
-          from D_EA_APPLICATION@eaist_mos_shard d
-          join(select win.*, rank() over (partition by journal_number, oos_reg_number, oos_ftp_protocol_id order by price, added_at desc) rnk
+          select distinct 
+            win.ID||4||(CASE WHEN l.joint_auction=1 or l.included_joint_auction=1 then l.id end) winid, 
+            win.ID||4||(CASE WHEN l.joint_auction=1 or l.included_joint_auction=1 then l.id end), 
+            d.APP_DATE, 
+            win.PRICE, 
+            V_ID_DATA_SOURCE id_Data_source, 
+            V_VERSION_DATE version_date 
+          from (select win.*, t.id tender_id, rank() over (partition by journal_number, oos_reg_number, oos_ftp_protocol_id order by price, added_at desc) rnk
                 from d_ea_winner@eaist_mos_shard win 
-                join d_procedure_entity@eaist_mos_shard pe on win.procedure_id=pe.id and pe.deleted_date is null and pe.published_id is not null) win
-          on d.journal_number=win.journal_number and d.oos_reg_number=win.oos_reg_number and d.oos_ftp_protocol_id=win.oos_ftp_protocol_id and win.rnk=1
-          join (select id, registry_number from t_tender where version_date= V_VERSION_DATE and id_data_source= V_ID_DATA_SOURCE) t on d.oos_reg_number=t.registry_number
-          join (select id, tender_Id, joint_auction, included_joint_auction from t_lot where version_date= V_VERSION_DATE and id_data_source= V_ID_DATA_SOURCE) l on l.tender_id=t.id;
+                join t_tender t on win.procedure_id=t.entity_id and t.id_data_source=V_ID_DATA_SOURCE and t.version_date=V_VERSION_DATE) win
+          join (select id, tender_Id, joint_auction, included_joint_auction from t_lot where version_date= V_VERSION_DATE and id_data_source= V_ID_DATA_SOURCE) l on l.tender_id=win.tender_id and win.rnk=1
+          left join (select journal_number, oos_reg_number, oos_ftp_protocol_id, min(app_date) app_date 
+                     from D_EA_APPLICATION@eaist_mos_shard
+                     group by journal_number, oos_reg_number, oos_ftp_protocol_id) d
+          on d.journal_number=win.journal_number and d.oos_reg_number=win.oos_reg_number and d.oos_ftp_protocol_id=win.oos_ftp_protocol_id;
     -- Привязка кол-ва обработанных строк
     :V_ROWCOUNT := SQL%ROWCOUNT;
 
